5daf6c827a10895e652e7cfc172b7669fd9ad9d0
Jonathan Hsieh <jonathan@Jonathans-MacBook-Pro.local>
2019-07-08 12:16:25 -0700

    Finish server side get request for endpoint "/reservation"

--- server/index.js
+++ server/index.js
@@ -2,9 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const db = require('./db/index');
 
 app.use(express.static(path.resolve(__dirname, '..', 'client','dist')));
 
+app.get('/reservation', (req, res) => {
+  const {restaurant_id, startTimestamp, endTimestamp} = req.params;
+  const query = `SELECT * FROM reservation WHERE restaurant = ${restaurant_id} AND timestamp BETWEEN ${startTimestamp} AND ${endTimestamp}`;
+  db.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(results);
+    }
+  })
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
-})
+});
