85d637820dd0d8c5e852d33a824f77ac85b7032e
Jonathan Hsieh <jonathan@Jonathans-MacBook-Pro.local>
2019-07-09 20:06:17 -0700

    Create basic structure for App.jsx

---
+++ client/src/component/Calendar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>I'm a Calendar</div>
+    )
+  }
+}
+
+export default Calendar;
