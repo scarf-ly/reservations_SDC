ab04ffb7d75d835d474808725f5d003ac2781c62
Keaton Tatooles <keatontatooles@Keatons-MacBook-Air.local>
2019-07-25 10:45:08 -0700

    finished reservation generation script, need to edit timestampe randomizer

--- server/db/reservationGeneration.js
+++ server/db/reservationGeneration.js
@@ -1,26 +1,7 @@
 // \copy reservations (restaurant_id, moment, reservation_size) FROM '/Users/keatontatooles/Documents/sdc/reservation/server/db/reservations.csv' DELIMITER ',' CSV;
 const fs = require('fs');
 
-// const arrayCsv = require("array-csv");
-
-// const arrayWithoutHeader = [];
-
-// while (arrayWithoutHeader.length < 20) {
-//   const newEntry = [];
-//   newEntry.push(Math.floor(Math.random()*10), Math.floor(Math.random()*100));
-//   arrayWithoutHeader.push(newEntry);
-// }
-
-
-// arrayCsv.array2dToCsv(arrayWithoutHeader, "reservations.csv", {hasHeader: false});
-
 const myWriteStream = fs.createWriteStream('reservations.csv');
-// for (let i = 0; i < 5; i++) {
-//     let data =[1+ Math.floor(Math.random()*10), Math.floor(Math.random()*100), Math.floor(Math.random()*100)].join(',')
-//     data += '\n';
-//     myWriteStream.write(data, 'utf8');
-// }
-// myWriteStream.end();
 
 let data =[1+ Math.floor(Math.random()*10), Math.floor(Math.random()*100), Math.floor(Math.random()*100)].join(',')
 function writeOneMillionTimes(myWriteStream, data, encoding, callback) {
