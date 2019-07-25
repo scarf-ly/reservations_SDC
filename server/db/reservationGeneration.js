9877bb4ab30b1e9622712b9fe559f4c51a8f9f23
Keaton Tatooles <keatontatooles@Keatons-MacBook-Air.local>
2019-07-24 22:36:02 -0700

    used writeStream to make async csv

--- server/db/reservationGeneration.js
+++ server/db/reservationGeneration.js
@@ -1,15 +1,52 @@
-// \copy echo FROM '/Users/keatontatooles/Documents/sdc/reservation/server/db/reservations.csv' DELIMITER ',' CSV;
+// \copy reservations (restaurant_id, moment, reservation_size) FROM '/Users/keatontatooles/Documents/sdc/reservation/server/db/reservations.csv' DELIMITER ',' CSV;
+const fs = require('fs');
+
+// const arrayCsv = require("array-csv");
+
+// const arrayWithoutHeader = [];
+
+// while (arrayWithoutHeader.length < 20) {
+//   const newEntry = [];
+//   newEntry.push(Math.floor(Math.random()*10), Math.floor(Math.random()*100));
+//   arrayWithoutHeader.push(newEntry);
+// }
 
 
-const arrayCsv = require("array-csv");
+// arrayCsv.array2dToCsv(arrayWithoutHeader, "reservations.csv", {hasHeader: false});
 
-const arrayWithoutHeader = [];
+const myWriteStream = fs.createWriteStream('reservations.csv');
+// for (let i = 0; i < 5; i++) {
+//     let data =[1+ Math.floor(Math.random()*10), Math.floor(Math.random()*100), Math.floor(Math.random()*100)].join(',')
+//     data += '\n';
+//     myWriteStream.write(data, 'utf8');
+// }
+// myWriteStream.end();
 
-while (arrayWithoutHeader.length < 20) {
-  const newEntry = [];
-  newEntry.push(Math.floor(Math.random()*10), Math.floor(Math.random()*100));
-  arrayWithoutHeader.push(newEntry);
+let data =[1+ Math.floor(Math.random()*10), Math.floor(Math.random()*100), Math.floor(Math.random()*100)].join(',')
+function writeOneMillionTimes(myWriteStream, data, encoding, callback) {
+    let i = 50000000;
+    write();
+    function write() {
+      let ok = true;
+      do {
+        i--;
+        data =[1+ Math.floor(Math.random()*10), Math.floor(Math.random()*100), Math.floor(Math.random()*100)].join(',')
+        data += '\n';
+        if (i === 0) {
+          // Last time!
+          myWriteStream.write(data, encoding, callback);
+        } else {
+          // See if we should continue, or wait.
+          // Don't pass the callback, because we're not done yet.
+          ok = myWriteStream.write(data, encoding);
+        }
+      } while (i > 0 && ok);
+      if (i > 0) {
+        // Had to stop early!
+        // Write some more once it drains.
+        myWriteStream.once('drain', write);
+      }
+    }
 }
 
-
-arrayCsv.array2dToCsv(arrayWithoutHeader, "reservations.csv", {hasHeader: false});
+writeOneMillionTimes(myWriteStream, data, 'utf8');
